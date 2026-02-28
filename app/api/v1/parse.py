1421ed640ba68fa301bb6737c301794bdbcbcc58
Kikos1337 <skalisusov@inbox.ru>
2026-02-28 04:15:33 +0300

    move get_session

--- app/api/v1/parse.py
+++ app/api/v1/parse.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.session import async_session_maker
 from app.services.parser import parse_and_store
+from app.db.dependencies import get_session
 
 router = APIRouter(prefix="/parse", tags=["parser"])
 
-
-async def get_session() -> AsyncSession:
-    async with async_session_maker() as session:
-        yield session
 
 
 @router.post("/")
